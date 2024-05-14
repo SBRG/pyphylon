f7392a030a3a2bf06301eb75ff20e976d3f7cf5d
Siddharth M Chauhan <github.chauhan.siddharth@gmail.com>
2024-05-14 10:06:02 -0700

    initial commit

---
+++ pyphylon/io.py
@@ -0,0 +1,18 @@
+"""
+Functions for reading and writing data into files.
+"""
+
+import gzip
+import json
+
+import pandas as pd
+
+from pyphylon.core import NmfData
+
+def save_to_json(data, filename, compress=False):
+    # TODO: Implement this function
+    pass
+
+def load_json_model(filename):
+    # TODO: Implement this function
+    pass
