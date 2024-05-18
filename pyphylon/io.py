eb87c83ba9c4e4dac67e2ada49b5b2ec27cf6886
Siddharth M Chauhan <github.chauhan.siddharth@gmail.com>
2024-05-18 00:32:00 -0700

    feat: add pickle and json functions to io.py

--- pyphylon/io.py
+++ pyphylon/io.py
@@ -2,17 +2,81 @@
 Functions for reading and writing data into files.
 """
 
-import gzip
 import json
-
+import pickle
 import pandas as pd
 
-from pyphylon.core import NmfData
+from .core import NmfData
+from .models import NmfModel
 
-def save_to_json(data, filename, compress=False):
-    # TODO: Implement this function
-    pass
+def save_nmf_data(nmf_data, filepath):
+    """
+    Save an NmfData object to a file.
 
-def load_json_model(filename):
-    # TODO: Implement this function
-    pass
+    Parameters:
+    - nmf_data (NmfData): The NmfData object to save.
+    - filepath (str): The path to the file where the object will be saved.
+    """
+    with open(filepath, 'wb') as f:
+        pickle.dump(nmf_data, f)
+
+def load_nmf_data(filepath):
+    """
+    Load an NmfData object from a file.
+
+    Parameters:
+    - filepath (str): The path to the file from which to load the object.
+
+    Returns:
+    - NmfData: The loaded NmfData object.
+    """
+    with open(filepath, 'rb') as f:
+        return pickle.load(f)
+
+def save_nmf_model(nmf_model, filepath):
+    """
+    Save an NmfModel object to a file.
+
+    Parameters:
+    - nmf_model (NmfModel): The NmfModel object to save.
+    - filepath (str): The path to the file where the object will be saved.
+    """
+    with open(filepath, 'wb') as f:
+        pickle.dump(nmf_model, f)
+
+def load_nmf_model(filepath):
+    """
+    Load an NmfModel object from a file.
+
+    Parameters:
+    - filepath (str): The path to the file from which to load the object.
+
+    Returns:
+    - NmfModel: The loaded NmfModel object.
+    """
+    with open(filepath, 'rb') as f:
+        return pickle.load(f)
+
+def save_json(data, filepath):
+    """
+    Save data to a JSON file.
+
+    Parameters:
+    - data (dict): The data to save.
+    - filepath (str): The path to the file where the data will be saved.
+    """
+    with open(filepath, 'w') as f:
+        json.dump(data, f)
+
+def load_json(filepath):
+    """
+    Load data from a JSON file.
+
+    Parameters:
+    - filepath (str): The path to the file from which to load the data.
+
+    Returns:
+    - dict: The loaded data.
+    """
+    with open(filepath, 'r') as f:
+        return json.load(f)
