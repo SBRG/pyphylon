3cd599b337c529fc663bbc2d38d3d0e0220319fe
Siddharth M Chauhan <github.chauhan.siddharth@gmail.com>
2024-05-23 22:49:20 -0700

    feat: add genereate_pairwise_distances

--- pyphylon/mash.py
+++ pyphylon/mash.py
@@ -12,22 +12,43 @@ import scipy.spatial as sp
 
 from kneebow.rotor import Rotor
 
-def mash_sketch_genomes(genome_dir, output_file):
+def sketch_genomes(genome_dir, output_file):
     """
     Generate a Mash sketch file.
 
     Parameters:
     - genome_dir (str): Path to genome files for Mash analysis
-    - output_file (str): Path for output file. If non
+    - output_file (str): Path for output Mash sketch file.
     """
     # Configure logging
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    # logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     # Generate command
     cmd = [
         "mash", "sketch",
         "-o", f"{output_file}",
         f"{genome_dir}"
+    ]
+
+    # Run command
+    result = subprocess.run(
+        cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    return result
+
+def generate_pairwise_distances(mash_sketch_file, output_file):
+    """
+    Generate a table of all pairwise Mash distances.
+
+    Parameters:
+    - mash_sketch_file (str): Path to Mash sketch file
+    - output_file (str): Path for output Mash distances table.
+    """
+    # Generate command
+    cmd = [
+        "mash", "dist",
+        f"{mash_sketch_file}", "{mash_sketch_file}", ">", f"{output_file}"
     ]
 
     # Run command
